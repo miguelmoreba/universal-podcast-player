e763b07b7302fe0eee8cdfdc6e4089fa3b9417a5
Miguel Moreno Baladrón <miguelmorenobaladron@Miguels-MacBook-Air.local>
2021-08-08 23:30:38 +0100

    Add functionality to dowload file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,23 @@ import { FormControl } from '@angular/forms';
 export class AppComponent {
   title = 'ng-audio-cutter';
   link = new FormControl("")
+
+
+  async onButtonClick() {
+
+    var a = document.createElement("a");
+    document.body.appendChild(a);
+
+    console.log("Heeeey");
+    const file = await fetch(this.link.value, { headers: { "Access-Control-Allow-Origin": "*" } })
+      .then(r => r.blob())
+      .then(blobFile => new File([blobFile], "fileNameGoesHere", { type: "audio/mp3" }))
+
+    const url = window.URL.createObjectURL(file)
+
+    a.href = url;
+    a.download = "whatever";
+    a.click();
+    window.URL.revokeObjectURL(url);
+  }
 }
