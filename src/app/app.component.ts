5785da29ee9a162976bfd19f3223d38229c23641
Miguel Moreno Baladrón <miguelmorenobaladron@Miguels-MacBook-Air.local>
2021-08-13 20:51:08 +0100

    Working, url can be downloaded and local file can be listened to without issues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,25 +9,58 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  constructor(
+    private httpClient: HttpClient,
+    private sanitizer: DomSanitizer
+    ) { }
+
   title = 'ng-audio-cutter';
   link = new FormControl("")
+  local_url: any
 
 
-  async onButtonClick() {
+  async onUrlButtonClick() {
 
-    var a = document.createElement("a");
-    document.body.appendChild(a);
+    this.httpClient.get(this.link.value, { responseType: 'blob' })
+      .subscribe(response => {
+        const a = document.createElement("a");
+        document.body.appendChild(a);
+        const url = URL.createObjectURL(response)
+        console.log(url)
+        a.href = url;
+        a.download = "whatever";
+        a.click();
+        window.URL.revokeObjectURL(url);
+      }
+    );
+  }
 
-    console.log("Heeeey");
-    const file = await fetch(this.link.value, { headers: { "Access-Control-Allow-Origin": "*" } })
-      .then(r => r.blob())
-      .then(blobFile => new File([blobFile], "fileNameGoesHere", { type: "audio/mp3" }))
+  onInputChange(myEvent: any) {
+    if (myEvent.target.files && myEvent.target.files[0]) {
+      console.log("heoihjfasdkljhf")
+      let file = myEvent.target.files[0];
+      let fr = new FileReader();
+      console.log("I get to here too")
+      fr.onload = (event: any) => {
+        let base64 = event.target.result
+        let BASE64_MARKER = ';base64,';
+        let base64Index = event.target.result.indexOf(BASE64_MARKER) + BASE64_MARKER.length;
+        base64 = event.target.result.substring(base64Index);
+        let raw = window.atob(base64);
+        let rawLength = raw.length;
+        let array = new Uint8Array(new ArrayBuffer(rawLength));
 
-    const url = window.URL.createObjectURL(file)
+        for (let i = 0; i < rawLength; i++) {
+          array[i] = raw.charCodeAt(i);
+        }
 
-    a.href = url;
-    a.download = "whatever";
-    a.click();
-    window.URL.revokeObjectURL(url);
+        const blob = new Blob([array], { type: 'audio/mp3' });
+        const blobUrl = URL.createObjectURL(blob);
+        console.log(blobUrl)
+        this.local_url = this.sanitizer.bypassSecurityTrustResourceUrl(blobUrl)
+      }
+      fr.readAsDataURL(file)
+    }
   }
 }
