64ad92bba8638f583c040da5aac8e8f063f932ff
Miguel Moreno Baladrón <miguelmorenobaladron@Miguels-MacBook-Air.local>
2021-10-19 16:17:08 +0100

    Add audio element reference, hide controls and add play/pause button

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,7 +6,7 @@ import { Router } from '@angular/router';
   templateUrl: './player.component.html',
   styleUrls: ['./player.component.scss']
 })
-export class PlayerComponent {
+export class PlayerComponent implements OnInit {
 
   @Input() url = '';
   @Input() originalUrl = '';
@@ -14,11 +14,16 @@ export class PlayerComponent {
   end = ''
   duration = 0;
   currentHigh = 0;
+  @ViewChild('audio_player') audioPlayer: any;
 
   constructor(
     private readonly router: Router
   ){
 
+  }
+  ngOnInit(): void {
+    const player = <HTMLAudioElement>document.getElementById('audio-player');
+    player.controls = false;
   }
 
   timeUpdate(myEvent: any) {
@@ -30,13 +35,17 @@ export class PlayerComponent {
 
   onStartChange(myEvent: any) {
     const player = <HTMLAudioElement>document.getElementById('audio-player');
-    // console.log(myEvent.target.value)
     player.currentTime = myEvent.target.value
   }
 
   setDuration(myEvent: any) {
     this.duration = Math.round(myEvent.currentTarget.duration);
     this.currentHigh = this.duration
+  }
+
+  onPlayPause() {
+    const player = <HTMLAudioElement>this.audioPlayer.nativeElement;
+    player.paused ? player.play() : player.pause()
   }
 
   onShare(){
